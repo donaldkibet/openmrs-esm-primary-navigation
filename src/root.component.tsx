35adec6cc53264635ba69c82e7d97e1c1b03622b
Donald Kibet <chelashawdonald@yahoo.com>
2019-09-18 14:27:26 +0300

    MF-70 : Adding home icon to navbar

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,6 +1,19 @@
 import React from "react";
 import openmrsRootDecorator from "@openmrs/react-root-decorator";
 import styles from "./root.styles.css";
+import { Link, BrowserRouter as Router, Route } from "react-router-dom";
+
+function ShowHomeIcon() {
+  return (
+    <Router>
+      <Link to="/openmrs/spa/home">
+        <svg className={`omrs-icon ${styles.menuIcon}`}>
+          <use xlinkHref="#omrs-icon-home" />
+        </svg>
+      </Link>
+    </Router>
+  );
+}
 
 function Root() {
   const [sidenavOpen, setSidenavOpen] = React.useState(false);
@@ -16,18 +29,9 @@ function Root() {
 
   return (
     <nav className={styles.topNav}>
-      <button
-        className="omrs-unstyled omrs-padding-left-4 omrs-padding-right-4"
-        onClick={toggleSidenav}
-      >
-        <svg className={`omrs-icon ${styles.menuIcon}`}>
-          <use xlinkHref="#omrs-icon-menu" />
-        </svg>
-      </button>
-      <div className="omrs-type-title-4">
-        {/* We'll figure out whether to use document.title or not later */}
-        Home
-      </div>
+      <Router>
+        <Route path="/openmrs/spa/home/" strict component={ShowHomeIcon} />
+      </Router>
       <div className={styles.avatar}>AB</div>
     </nav>
   );
